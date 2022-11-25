da79caf48278385dfb9f6ec2c40f2c923d5e76f0
Jason <jason.afonso.fernandes@gmail.com>
2022-11-25 16:57:01 +0100

    Séparation DockerFIle

--- init.sh
+++ init.sh
@@ -1,19 +1,19 @@
 
-
+#lancement du docker-compose
 docker-compose up -d
 
+#php1 conteneur
 docker-compose exec php1 /bin/sh -c "composer install"
-
 docker-compose exec php1 php artisan key:generate
 docker-compose exec php1 php artisan config:cache
 docker-compose exec php1 php artisan migrate:fresh --seed
+docker-compose exec php1 npm install
+docker-compose exec php1 npm run build
 
+#php2 conteneur
 docker-compose exec php2 /bin/sh -c "composer install"
 docker-compose exec php2 php artisan key:generate
 docker-compose exec php2 php artisan config:cache
 docker-compose exec php2 php artisan migrate:fresh --seed
-
-docker-compose exec php1 npm install
-docker-compose exec php1 npm run build
 docker-compose exec php2 npm install
 docker-compose exec php2 npm run build
