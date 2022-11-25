712c17a9644b7d4d94225599c00c33f14d925e46
Jason <jason.afonso.fernandes@gmail.com>
2022-11-25 15:53:32 +0100

    env et init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
+
+
 docker-compose up -d
 
 docker-compose exec php1 /bin/sh -c "composer install"
@@ -10,3 +12,8 @@ docker-compose exec php2 /bin/sh -c "composer install"
 docker-compose exec php2 php artisan key:generate
 docker-compose exec php2 php artisan config:cache
 docker-compose exec php2 php artisan migrate:fresh --seed
+
+docker-compose exec php1 npm install
+docker-compose exec php1 npm run build
+docker-compose exec php2 npm install
+docker-compose exec php2 npm run build
