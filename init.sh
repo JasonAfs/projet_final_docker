2245e201591a3b3a4976476595c3e32046c96324
Jason <jason.afonso.fernandes@gmail.com>
2022-11-25 14:36:22 +0100

    final bash script

--- init.sh
+++ init.sh
@@ -1,12 +1,12 @@
 docker-compose up -d
 
-docker-compose exec php1 /bin/sh composer install
+docker-compose exec php1 /bin/sh -c "composer install"
 
 docker-compose exec php1 php artisan key:generate
 docker-compose exec php1 php artisan config:cache
 docker-compose exec php1 php artisan migrate:fresh --seed
 
-docker-compose exec php2 /bin/sh composer install
+docker-compose exec php2 /bin/sh -c "composer install"
 docker-compose exec php2 php artisan key:generate
 docker-compose exec php2 php artisan config:cache
 docker-compose exec php2 php artisan migrate:fresh --seed
